152bd33ec95421c6272c0a59110aabcaf015ea0b
ZARIFMAC <uzbzarif@gmail.com>
2024-01-27 15:55:45 +0500

    log

--- app.py
+++ app.py
@@ -10,5 +10,6 @@ chat_id = '5575549228'
 # route for index page
 @app.route('/', methods=['POST'])
 def index():
+    print('index page')
     # bot.send_message(chat_id=chat_id, text='Hello World!!!')
     return 'index page'
