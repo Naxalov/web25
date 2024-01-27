74769dc883bc933ef5b8253a274a6f7b580f07d8
ZARIFMAC <uzbzarif@gmail.com>
2024-01-27 15:13:58 +0500

    send MSG

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+import telegram
 # Create the application instance
 app = Flask(__name__)
+
+TOKEN ='6534122111:AAGyHhbsCKcIu391JBLSiCZfjmq-SbG97Z0'
+bot = telegram.Bot(TOKEN)
+chat_id = '5575549228'
 
 # route for index page
 @app.route('/')
 def index():
+    bot.send_message(chat_id=chat_id, text='Hello World!!!')
     return 'Hello World!!!'
